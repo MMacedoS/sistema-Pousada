f534e7f0a2bf630829f725a4652d055d3b4800dd
Mauricio Macedo da Silva <mauricio.silva@fepese.org.br>
2025-08-14 10:12:57 -0300

    feat: correção de nome de classe

---
+++ App/Transformers/Cashbox/TransacaoCaixaTransformer.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Transformers\Cashbox;
+
+use App\Models\Cashbox\TransacaoCaixa;
+
+class TransacaoCaixaTransformer
+{
+
+    public function transform(TransacaoCaixa $transacao)
+    {
+        if (!$transacao) {
+            return null;
+        }
+
+        if (is_array($transacao)) {
+            $transacao = (object) $transacao;
+        }
+
+        return [
+            'code' => (int)$transacao->id,
+            'id' => $transacao->uuid,
+            'type' => $transacao->type,
+            'description' => $transacao->description,
+            'payment_form' => $transacao->payment_form,
+            'canceled' => (bool)$transacao->canceled,
+            'amount' => (float)$transacao->amount ?? 0,
+            'created_at' => $transacao->created_at,
+        ];
+    }
+
+    public function transformCollection(array $transacoes)
+    {
+        return array_map(fn($transacao) => $this->transform($transacao), $transacoes);
+    }
+}
