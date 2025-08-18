3a2cec81a13348f7522972ffbfbea62e2e198878
Mauricio Macedo da Silva <mauricio.silva@fepese.org.br>
2025-08-18 09:37:32 -0300

    feat: ajustado traits

--- App/Http/Controllers/Controller.php
+++ App/Http/Controllers/Controller.php
@@ -4,14 +4,20 @@ namespace App\Http\Controllers;
 
 use App\Config\Router;
 use App\Config\Session;
+use App\Http\Controllers\v1\Traits\GenericTrait;
+use App\Http\Controllers\v1\Traits\UserToPerson;
+use App\Http\Controllers\Traits\HasPermissions;
 
-class Controller {
-  
+class Controller
+{
+
+    use GenericTrait, UserToPerson, HasPermissions;
+
     public $router;
     protected $session;
     protected $view;
 
-    public function __construct() 
+    public function __construct()
     {
         $this->router = new Router();
         $this->session = new Session();
